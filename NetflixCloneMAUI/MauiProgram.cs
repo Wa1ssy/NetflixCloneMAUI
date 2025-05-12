6213f03228095a485743c55cd7a2d4006f30d567
Opilane TTHK <132674823+Wa1ssy@users.noreply.github.com>
2025-05-12 15:57:03 +0300

    part 1+ 2 combined

---
+++ NetflixCloneMAUI/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using NetflixCloneMAUI.Pages;
+using NetflixCloneMAUI.Services;
+using NetflixCloneMAUI.ViewModels;
+
+namespace NetflixCloneMAUI;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Poppins-Regular.ttf", "PoppinsRegular");
+				fonts.AddFont("Poppins-Semibold.ttf", "PoppinsSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+        builder.Services.AddHttpClient(TmdbService.TmdbHttpClientName,
+            httpClient => httpClient.BaseAddress = new Uri("https://api.themoviedb.org"));
+
+        builder.Services.AddSingleton<TmdbService>();
+        builder.Services.AddSingleton<HomeViewModel>();
+        builder.Services.AddSingleton<MainPage>();
+
+        return builder.Build();
+	}
+}
